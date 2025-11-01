94b1ed08a45a716ca13ad0e7bdc63ad69d86a819
amitbishnoii <amitbishnoi29012@gmail.com>
2025-11-01 17:21:51 +0530

    update styling of login page and made basic structure

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import "../CSS/Login.css"
+import { useForm } from "react-hook-form"
+
+const onSubmit = (data) => {
+    console.log(data);
+}
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit
+    } = useForm()
+
     return (
         <>
-            <div className="card">
-                card
+            <div className="main">
+                <div className="login-card">
+                    <h1>Login</h1>
+                    <div className="login-form">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input {...register('username')} required={true} placeholder='Username' />
+                            <br />
+                            <input {...register('password')} required={true} placeholder='Password' />
+                            <br />
+                            <div className="buttons">
+                                <button type='submit'>Login</button>
+                                <button>Sign-up</button>
+                            </div>
+                        </form >
+                    </div >
+                </div>
             </div>
         </>
     )
