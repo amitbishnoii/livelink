6efb6f38492896bcff0dec05fd8d81173fd11aee
amitbishnoii <amitbishnoi29012@gmail.com>
2025-11-01 16:25:22 +0530

    setup basic structure

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../CSS/Login.css"
+
+const Login = () => {
+    return (
+        <>
+            <div className="card">
+                card
+            </div>
+        </>
+    )
+}
+
+export default Login
