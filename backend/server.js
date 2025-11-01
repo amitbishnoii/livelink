d11d9766cc9ae0c30041f3f749aa7a86de3434c2
amitbishnoii <amitbishnoi29012@gmail.com>
2025-11-01 18:30:19 +0530

    setup backend structure and connect with mongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js";
+import cors from "cors"
 
-
+dotenv.config()
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
 
 app.get("/", async (req, res) => {
     res.send("hello world")    
