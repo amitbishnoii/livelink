5227c9737a96730753b765d734d37ad6f22f6034
amitbishnoii <amitbishnoi29012@gmail.com>
2025-11-07 20:00:12 +0530

    dynamically load the friend list from friend state

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import "../CSS/Chat.css"
+import { useState } from 'react'
 
 const Chat = () => {
+
+    const [friend, setfriend] = useState([
+        {"name": "avrit",},
+        {"name": "avrit",},
+        {"name": "avrit",},
+        {"name": "avrit",},
+        {"name": "avrit",},
+    ])
+
+
     return (
         <>
             <div className="chat-main">
                 <h2>LiveLink Chat Application</h2>
                 <div className="content-main">
                     <div className="left-bar">
+                        <h3>Messages</h3>
                         <div className="search-bar">
                             <input type="text" placeholder='Search' />
+                            <button>Search</button>
+                        </div>
+                        <div className="friends-section">
+                            {friend.map((info) => {
+                                return (
+                                    <div className="profile-card">
+                                        <p>{info.name}</p>
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                     <div className="center-main">
-                        this is main chat area
+                        <span>Search for friends to start chatting...</span>
                     </div>
                     <div className="right-bar">
                         this is right area
