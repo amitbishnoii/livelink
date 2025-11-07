62ee5e0ef9bdae03e64c2b76aa4a1ca5f14d6573
amitbishnoii <amitbishnoi29012@gmail.com>
2025-11-07 20:46:51 +0530

    update styles of the chat page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,14 +3,19 @@ import "../CSS/Chat.css"
 import { useState } from 'react'
 
 const Chat = () => {
+    const [selectedUser, setselectedUser] = useState(null);
 
     const [friend, setfriend] = useState([
-        {"name": "avrit",},
-        {"name": "avrit",},
-        {"name": "avrit",},
-        {"name": "avrit",},
-        {"name": "avrit",},
-    ])
+        {
+            "name": "avrit",
+            "profile-pic": "https://imgs.search.brave.com/OeaPDkJ0ITqvZfJOZippVCiinWetnZAvuLEYObrzLPc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI3/MDIxODIxMC9waG90/by9jbG9zZS11cC1j/b21wdXRlci1jb2Rl/LW9uLXNjcmVlbi5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/QUl2bklmWG9Eangt/ZW11OG9rRWtpMTNa/MVpBZ1B1WnFBOFhU/al9hTC1tST0",
+            "bio": "a;ldfjas;ldkfj"
+        },
+        {
+            "name": "rahul",
+            "profile-pic": "https://imgs.search.brave.com/OeaPDkJ0ITqvZfJOZippVCiinWetnZAvuLEYObrzLPc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI3/MDIxODIxMC9waG90/by9jbG9zZS11cC1j/b21wdXRlci1jb2Rl/LW9uLXNjcmVlbi5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/QUl2bklmWG9Eangt/ZW11OG9rRWtpMTNa/MVpBZ1B1WnFBOFhU/al9hTC1tST0",
+        },
+    ]);
 
 
     return (
@@ -25,9 +30,10 @@ const Chat = () => {
                             <button>Search</button>
                         </div>
                         <div className="friends-section">
-                            {friend.map((info) => {
+                            {friend.map((info, i) => {
                                 return (
-                                    <div className="profile-card">
+                                    <div key={i} className="profile-card" onClick={() => setselectedUser(info)}>
+                                        <img src={info['profile-pic']} alt={info.name} className='profile-pic' />
                                         <p>{info.name}</p>
                                     </div>
                                 );
@@ -35,10 +41,21 @@ const Chat = () => {
                         </div>
                     </div>
                     <div className="center-main">
-                        <span>Search for friends to start chatting...</span>
+                        {selectedUser ? (
+                            <div className="chat-profile">
+                                <img src={selectedUser["profile-pic"]} alt="" />
+                                <p>{selectedUser.name}</p>
+                            </div>
+                        ) : <span>Search for friends to start chatting...</span>}
                     </div>
                     <div className="right-bar">
-                        this is right area
+                        {selectedUser ? (
+                            <div className="user-info">
+                                <img src={selectedUser["profile-pic"]} alt="" />
+                                <h5>{selectedUser.name}</h5>
+                                <p>{selectedUser.bio}</p>
+                            </div>
+                        ) : ""}
                     </div>
                 </div>
             </div>
