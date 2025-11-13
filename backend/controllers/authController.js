68a895d4671f6eae0d9d203a264e3238682c1fe8
amitbishnoii <amitbishnoi29012@gmail.com>
2025-11-13 21:07:21 +0530

    implement the feature to check if both the users are already friends

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -73,15 +73,18 @@ export const addFriends = async (req, res) => {
     try {
         const { sender_id, rec_id } = req.body;
         const sender = await User.findOne({ _id: sender_id });
+        const sender_friends = await Friends.findOne({ user: sender_id });
         const reciever = await User.findOne({ _id: rec_id });
 
-        if (sender_id === rec_id) {
+        if (sender_friends.friendList.includes(rec_id)) {
+            res.json({ message: "Both user are already friends!", success: null});
+        } else if (sender_id === rec_id) {
             res.json({ message: "Both IDs are Same!", success: false });
         } else if (!sender || !reciever) {
             res.json({ message: "User Dosen't Exist!", success: false });
         } else {
             const add_friend = await Friends.findOneAndUpdate({ user: sender_id }, {
-                $push: { friendList: rec_id }
+                $addToSet: { friendList: rec_id }
             })
             res.json({ message: "friend added!", info: add_friend, success: true })
         }
