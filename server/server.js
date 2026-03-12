f87ff5ee3c3b2a33597ddefded0722c741ba4041
amitbishnoii <amitbishnoi29012@gmail.com>
2026-03-12 23:09:05 +0530

    add vercel url to cors

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 const port = 3000;
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://chat-application-git-main-amitbishnoiis-projects.vercel.app",
         methods: ["GET", "POST"],
     }
 });
