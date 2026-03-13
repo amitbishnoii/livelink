66e03ea091746d187cae933601dad886612adaca
amitbishnoii <amitbishnoi29012@gmail.com>
2026-03-13 22:23:16 +0530

    add lazy loading to reduce bundle size

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Register from "./pages/Register"
-import Login from "./pages/Login"
-import Setup from "./pages/Setup"
-import Chat from "./pages/Chat"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Setup = lazy(() => import("./pages/Setup"));
+const Chat = lazy(() => import("./pages/Chat"));
 
 function App() {
-
-
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/setup" element={<Setup />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/" element={<Login />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/setup" element={<Setup />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </Suspense>
       </Router>
     </>
   )
